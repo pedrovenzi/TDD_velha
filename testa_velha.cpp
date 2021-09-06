1528de9effdb4993dbfdce07aa47949d1c497ce3
Pedro Venzi <pvenzi.pv@gmail.com>
2021-09-06 16:51:57 -0300

    Correcao nos nomes dos testes

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -10,7 +10,7 @@
   
 
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
+TEST_CASE( "Vitória 1", "[single-file]" ) {
 	int teste1[3][3]= {   { 0, 0, 1 }, 
 	                      { 0, 0, 1 },
 						  { 0, 0, 1 }
@@ -19,7 +19,7 @@ TEST_CASE( "Testa velha", "[single-file]" ) {
 
 }
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
+TEST_CASE( "Vitória 2", "[single-file]" ) {
 	int teste2[3][3]= {   { 0, 1, 0 }, 
 	                      { 0, 1, 0 },
 						  { 0, 1, 0 }
@@ -28,7 +28,7 @@ TEST_CASE( "Testa velha", "[single-file]" ) {
 
 }
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
+TEST_CASE( "Vitória 3", "[single-file]" ) {
 	int teste3[3][3]= {   { 1, 0, 0 }, 
 	                      { 1, 0, 0 },
 						  { 1, 0, 0 }
@@ -37,7 +37,7 @@ TEST_CASE( "Testa velha", "[single-file]" ) {
 
 }
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
+TEST_CASE( "Vitória 4", "[single-file]" ) {
 	int teste4[3][3]= {   { 1, 0, 0 }, 
 	                      { 0, 1, 0 },
 						  { 0, 0, 1 }
@@ -46,7 +46,7 @@ TEST_CASE( "Testa velha", "[single-file]" ) {
 
 }
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
+TEST_CASE( "Vitória 5", "[single-file]" ) {
 	int teste5[3][3]= {   { 0, 0, 1 }, 
 	                      { 0, 1, 0 },
 						  { 1, 0, 0 }
@@ -55,7 +55,7 @@ TEST_CASE( "Testa velha", "[single-file]" ) {
 
 }
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
+TEST_CASE( "Vitória 6", "[single-file]" ) {
 	int teste6[3][3]= {   { 0, 0, 0 }, 
 	                      { 0, 0, 0 },
 						  { 1, 1, 1 }
@@ -64,7 +64,7 @@ TEST_CASE( "Testa velha", "[single-file]" ) {
 
 }
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
+TEST_CASE( "Vitória 7", "[single-file]" ) {
 	int teste7[3][3]= {   { 0, 0, 0 }, 
 	                      { 1, 1, 1 },
 						  { 0, 0, 0 }
@@ -73,7 +73,7 @@ TEST_CASE( "Testa velha", "[single-file]" ) {
 
 }
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
+TEST_CASE( "Vitória 8", "[single-file]" ) {
 	int teste8[3][3]= {   { 1, 1, 1 }, 
 	                      { 0, 0, 0 },
 						  { 0, 0, 0 }
