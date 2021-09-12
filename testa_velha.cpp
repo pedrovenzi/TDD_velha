0d78680c9c8325bbe69809c2c8fde4764c7f714e
Pedro Venzi <pvenzi.pv@gmail.com>
2021-09-12 15:54:55 -0300

    Renomeando e Adequando Testes

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -10,73 +10,73 @@
   
 
 
-TEST_CASE( "Vitória 1", "[single-file]" ) {
-	int teste1[3][3]= {   { 0, 0, 1 }, 
-	                      { 0, 0, 1 },
-						  { 0, 0, 1 }
+TEST_CASE( "Vitória 1 - Coluna da Direita", "[single-file]" ) {
+	int teste1[3][3]= {   { 0, 2, 1 }, 
+	                      { 0, 2, 1 },
+						  { 2, 1, 1 }
 					  };
     REQUIRE( VerificaVelha(teste1) == 1 );
 
 }
 
-TEST_CASE( "Vitória 2", "[single-file]" ) {
-	int teste2[3][3]= {   { 0, 1, 0 }, 
-	                      { 0, 1, 0 },
-						  { 0, 1, 0 }
+TEST_CASE( "Vitória 2 - Coluna do Meio", "[single-file]" ) {
+	int teste2[3][3]= {   { 2, 1, 2 }, 
+	                      { 1, 1, 0 },
+						  { 2, 1, 0 }
 					  };
     REQUIRE( VerificaVelha(teste2) == 1 );
 
 }
 
-TEST_CASE( "Vitória 3", "[single-file]" ) {
-	int teste3[3][3]= {   { 1, 0, 0 }, 
-	                      { 1, 0, 0 },
-						  { 1, 0, 0 }
+TEST_CASE( "Vitória 3 - Coluna da Esquerda", "[single-file]" ) {
+	int teste3[3][3]= {   { 1, 2, 0 }, 
+	                      { 1, 1, 2 },
+						  { 1, 0, 2 }
 					  };
     REQUIRE( VerificaVelha(teste3) == 1 );
 
 }
 
-TEST_CASE( "Vitória 4", "[single-file]" ) {
-	int teste4[3][3]= {   { 1, 0, 0 }, 
-	                      { 0, 1, 0 },
-						  { 0, 0, 1 }
+TEST_CASE( "Vitória 4 - Diagonal Direita", "[single-file]" ) {
+	int teste4[3][3]= {   { 1, 2, 0 }, 
+	                      { 2, 1, 2 },
+						  { 0, 1, 1 }
 					  };
     REQUIRE( VerificaVelha(teste4) == 1 );
 
 }
 
-TEST_CASE( "Vitória 5", "[single-file]" ) {
-	int teste5[3][3]= {   { 0, 0, 1 }, 
-	                      { 0, 1, 0 },
-						  { 1, 0, 0 }
+TEST_CASE( "Vitória 5 - Diagonal Esquerda", "[single-file]" ) {
+	int teste5[3][3]= {   { 0, 2, 1 }, 
+	                      { 2, 1, 2 },
+						  { 1, 1, 0 }
 					  };
     REQUIRE( VerificaVelha(teste5) == 1 );
 
 }
 
-TEST_CASE( "Vitória 6", "[single-file]" ) {
-	int teste6[3][3]= {   { 0, 0, 0 }, 
-	                      { 0, 0, 0 },
+TEST_CASE( "Vitória 6 - Linha de Baixo", "[single-file]" ) {
+	int teste6[3][3]= {   { 2, 1, 2 }, 
+	                      { 0, 2, 0 },
 						  { 1, 1, 1 }
 					  };
     REQUIRE( VerificaVelha(teste6) == 1 );
 
 }
 
-TEST_CASE( "Vitória 7", "[single-file]" ) {
-	int teste7[3][3]= {   { 0, 0, 0 }, 
+TEST_CASE( "Vitória 7 - Linha do Meio", "[single-file]" ) {
+	int teste7[3][3]= {   { 2, 1, 0 }, 
 	                      { 1, 1, 1 },
-						  { 0, 0, 0 }
+						  { 2, 0, 2 }
 					  };
     REQUIRE( VerificaVelha(teste7) == 1 );
 
 }
 
-TEST_CASE( "Vitória 8", "[single-file]" ) {
+TEST_CASE( "Vitória 8 - Linha de Cima", "[single-file]" ) {
 	int teste8[3][3]= {   { 1, 1, 1 }, 
-	                      { 0, 0, 0 },
-						  { 0, 0, 0 }
+	                      { 2, 0, 2 },
+						  { 0, 2, 1 }
 					  };
     REQUIRE( VerificaVelha(teste8) == 1 );
 
