2a7e6145247b512bd2982f17ffa943f5d06f97f5
Pedro Venzi <pvenzi.pv@gmail.com>
2021-09-12 21:05:22 -0300

    Finalizacao e CPPLint

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -1,4 +1,4 @@
-
+// Copyright 2021 Pedro Venzi
 /**
  * \file testa_velha.cpp
  */
@@ -7,203 +7,181 @@
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-  
 
-TEST_CASE( "Vitória 1 - X Coluna da Direita", "[single-file]" ) {
-	int teste1[3][3]= {   { 0, 2, 1 }, 
-	                      { 0, 2, 1 },
-						  { 2, 1, 1 }
-					  };
-    REQUIRE( VerificaVelha(teste1) == 1 );
 
+TEST_CASE("Vitória 1 - X Coluna da Direita", "[single-file]") {
+  int teste1[3][3]= {   { 0, 2, 1 },
+                        { 0, 2, 1 },
+                        { 2, 1, 1 }
+  };
+    REQUIRE(VerificaVelha(teste1) == 1);
 }
 
-TEST_CASE( "Vitória 2 - X Coluna do Meio", "[single-file]" ) {
-	int teste2[3][3]= {   { 2, 1, 2 }, 
-	                      { 1, 1, 0 },
-						  { 2, 1, 0 }
-					  };
-    REQUIRE( VerificaVelha(teste2) == 1 );
-
+TEST_CASE("Vitória 2 - X Coluna do Meio", "[single-file]") {
+  int teste2[3][3]= {   { 2, 1, 2 },
+                        { 1, 1, 0 },
+                        { 2, 1, 0 }
+};
+    REQUIRE(VerificaVelha(teste2) == 1);
 }
 
-TEST_CASE( "Vitória 3 - X Coluna da Esquerda", "[single-file]" ) {
-	int teste3[3][3]= {   { 1, 2, 0 }, 
-	                      { 1, 1, 2 },
-						  { 1, 0, 2 }
-					  };
-    REQUIRE( VerificaVelha(teste3) == 1 );
-
+TEST_CASE("Vitória 3 - X Coluna da Esquerda", "[single-file]") {
+  int teste3[3][3]= {   { 1, 2, 0 },
+                        { 1, 1, 2 },
+                        { 1, 0, 2 }
+};
+    REQUIRE(VerificaVelha(teste3) == 1);
 }
 
-TEST_CASE( "Vitória 4 - X Diagonal Direita", "[single-file]" ) {
-	int teste12[3][3]= {  { 1, 2, 0 }, 
-	                      { 2, 1, 2 },
-						  { 0, 1, 1 }
-					  };
-    REQUIRE( VerificaVelha(teste12) == 1 );
-
+TEST_CASE("Vitória 4 - X Diagonal Direita", "[single-file]") {
+  int teste12[3][3]= {  { 1, 2, 0 },
+                        { 2, 1, 2 },
+                        { 0, 1, 1 }
+  };
+    REQUIRE(VerificaVelha(teste12) == 1);
 }
 
-TEST_CASE( "Vitória 5 - X Diagonal Esquerda", "[single-file]" ) {
-	int teste5[3][3]= {   { 0, 2, 1 }, 
-	                      { 2, 1, 2 },
-						  { 1, 1, 0 }
-					  };
-    REQUIRE( VerificaVelha(teste5) == 1 );
-
+TEST_CASE("Vitória 5 - X Diagonal Esquerda", "[single-file]") {
+  int teste5[3][3]= {   { 0, 2, 1 },
+                        { 2, 1, 2 },
+                        { 1, 1, 0 }
+    };
+    REQUIRE(VerificaVelha(teste5) == 1);
 }
 
-TEST_CASE( "Vitória 6 - X Linha de Baixo", "[single-file]" ) {
-	int teste6[3][3]= {   { 2, 1, 2 }, 
-	                      { 0, 2, 0 },
-						  { 1, 1, 1 }
-					  };
-    REQUIRE( VerificaVelha(teste6) == 1 );
-
+TEST_CASE("Vitória 6 - X Linha de Baixo", "[single-file]") {
+  int teste6[3][3]= {   { 2, 1, 2 },
+                        { 0, 2, 0 },
+                        { 1, 1, 1 }
+    };
+    REQUIRE(VerificaVelha(teste6) == 1);
 }
 
-TEST_CASE( "Vitória 7 - X Linha do Meio", "[single-file]" ) {
-	int teste7[3][3]= {   { 2, 1, 0 }, 
-	                      { 1, 1, 1 },
-						  { 2, 0, 2 }
-					  };
-    REQUIRE( VerificaVelha(teste7) == 1 );
-
+TEST_CASE("Vitória 7 - X Linha do Meio", "[single-file]") {
+  int teste7[3][3]= {   { 2, 1, 0 },
+                        { 1, 1, 1 },
+                        { 2, 0, 2 }
+    };
+    REQUIRE(VerificaVelha(teste7) == 1);
 }
 
-TEST_CASE( "Vitória 8 - X Linha de Cima", "[single-file]" ) {
-	int teste8[3][3]= {   { 1, 1, 1 }, 
-	                      { 2, 0, 2 },
-						  { 0, 2, 1 }
-					  };
-    REQUIRE( VerificaVelha(teste8) == 1 );
-
+TEST_CASE("Vitória 8 - X Linha de Cima", "[single-file]") {
+  int teste8[3][3]= {   { 1, 1, 1 },
+                        { 2, 0, 2 },
+                        { 0, 2, 1 }
+  };
+    REQUIRE(VerificaVelha(teste8) == 1);
 }
 
 
-TEST_CASE( "Vitória 9 - O Coluna da Direita", "[single-file]" ) {
-	int teste9[3][3]= {   { 0, 1, 2 }, 
-	                      { 0, 1, 2 },
-						  { 1, 2, 2 }
-					  };
-    REQUIRE( VerificaVelha(teste9) == 2 );
-
+TEST_CASE("Vitória 9 - O Coluna da Direita", "[single-file]") {
+  int teste9[3][3]= {   { 0, 1, 2 },
+                        { 0, 1, 2 },
+                        { 1, 2, 2 }
+  };
+    REQUIRE(VerificaVelha(teste9) == 2);
 }
 
-TEST_CASE( "Vitória 10 - O Coluna do Meio", "[single-file]" ) {
-	int teste10[3][3]= {   { 1, 2, 1 }, 
-	                      { 2, 2, 0 },
-						  { 1, 2, 0 }
-					  };
-    REQUIRE( VerificaVelha(teste10) == 2 );
-
+TEST_CASE("Vitória 10 - O Coluna do Meio", "[single-file]") {
+  int teste10[3][3]= {   { 1, 2, 1 },
+                         { 2, 2, 0 },
+                         { 1, 2, 0 }
+    };
+    REQUIRE(VerificaVelha(teste10) == 2);
 }
 
-TEST_CASE( "Vitória 11 - O Coluna da Esquerda", "[single-file]" ) {
-	int teste11[3][3]= {   { 2, 1, 0 }, 
-	                      { 2, 2, 1 },
-						  { 2, 0, 1 }
-					  };
-    REQUIRE( VerificaVelha(teste11) == 2 );
-
+TEST_CASE("Vitória 11 - O Coluna da Esquerda", "[single-file]") {
+  int teste11[3][3]= {   { 2, 1, 0 },
+                         { 2, 2, 1 },
+                         { 2, 0, 1 }
+  };
+    REQUIRE(VerificaVelha(teste11) == 2);
 }
 
-TEST_CASE( "Vitória 12 - O Diagonal Direita", "[single-file]" ) {
-	int teste12[3][3]= {   { 2, 1, 0 }, 
-	                      { 1, 2, 1 },
-						  { 0, 2, 2 }
-					  };
-    REQUIRE( VerificaVelha(teste12) == 2 );
-
+TEST_CASE("Vitória 12 - O Diagonal Direita", "[single-file]") {
+  int teste12[3][3]= {   { 2, 1, 0 },
+                         { 1, 2, 1 },
+                         { 0, 2, 2 }
+  };
+    REQUIRE(VerificaVelha(teste12) == 2);
 }
 
-TEST_CASE( "Vitória 13 - O Diagonal Esquerda", "[single-file]" ) {
-	int teste13[3][3]= {   { 0, 1, 2 }, 
-	                      { 1, 2, 1 },
-						  { 2, 2, 0 }
-					  };
-    REQUIRE( VerificaVelha(teste13) == 2 );
-
+TEST_CASE("Vitória 13 - O Diagonal Esquerda", "[single-file]") {
+  int teste13[3][3]= {   { 0, 1, 2 },
+                         { 1, 2, 1 },
+                         { 2, 2, 0 }
+  };
+    REQUIRE(VerificaVelha(teste13) == 2);
 }
 
-TEST_CASE( "Vitória 14 - O Linha de BaiOo", "[single-file]" ) {
-	int teste14[3][3]= {   { 1, 2, 1 }, 
-	                      { 0, 1, 0 },
-						  { 2, 2, 2 }
-					  };
-    REQUIRE( VerificaVelha(teste14) == 2 );
-
+TEST_CASE("Vitória 14 - O Linha de BaiOo", "[single-file]") {
+  int teste14[3][3]= {   { 1, 2, 1 },
+                         { 0, 1, 0 },
+                         { 2, 2, 2 }
+  };
+    REQUIRE(VerificaVelha(teste14) == 2);
 }
 
-TEST_CASE( "Vitória 15 - O Linha do Meio", "[single-file]" ) {
-	int teste15[3][3]= {   { 1, 2, 0 }, 
-	                      { 2, 2, 2 },
-						  { 1, 0, 1 }
-					  };
-    REQUIRE( VerificaVelha(teste15) == 2 );
-
+TEST_CASE("Vitória 15 - O Linha do Meio", "[single-file]") {
+  int teste15[3][3]= {   { 1, 2, 0 },
+                         { 2, 2, 2 },
+                         { 1, 0, 1 }
+  };
+    REQUIRE(VerificaVelha(teste15) == 2);
 }
 
-TEST_CASE( "Vitória 16 - O Linha de Cima", "[single-file]" ) {
-	int teste16[3][3]= {   { 2, 2, 2 }, 
-	                      { 1, 0, 1 },
-						  { 0, 1, 2 }
-					  };
-    REQUIRE( VerificaVelha(teste16) == 2 );
-
+TEST_CASE("Vitória 16 - O Linha de Cima", "[single-file]") {
+  int teste16[3][3]= {   { 2, 2, 2 },
+                         { 1, 0, 1 },
+                         { 0, 1, 2 }
+  };
+    REQUIRE(VerificaVelha(teste16) == 2);
 }
 
-TEST_CASE( "Jogo Impossível 1", "[single-file]" ) {
-	int teste17[3][3]= {   { 1, 1, 1 }, 
-	                      { 1, 2, 2 },
-						  { 0, 0, 0 }
-					  };
-    REQUIRE( VerificaVelha(teste17) == -2 );
-
+TEST_CASE("Jogo Impossível 1", "[single-file]") {
+  int teste17[3][3]= {   { 1, 1, 1 },
+                         { 1, 2, 2 },
+                         { 0, 0, 0 }
+  };
+    REQUIRE(VerificaVelha(teste17) == -2);
 }
 
-TEST_CASE( "Jogo Impossível 2", "[single-file]" ) {
-	int teste18[3][3]= {   { 1, 1, 1 }, 
-	                      { 2, 2, 2 },
-						  { 0, 0, 0 }
-					  };
-    REQUIRE( VerificaVelha(teste18) == -2 );
-
+TEST_CASE("Jogo Impossível 2", "[single-file]") {
+  int teste18[3][3]= {   { 1, 1, 1 },
+                         { 2, 2, 2 },
+                         { 0, 0, 0 }
+  };
+    REQUIRE(VerificaVelha(teste18) == -2);
 }
 
-TEST_CASE( "Jogo Impossível 3", "[single-file]" ) {
-	int teste19[3][3]= {   { 1, 1, 1 }, 
-	                      { 1, 2, 2 },
-						  { 1, 2, 2 }
-					  };
-    REQUIRE( VerificaVelha(teste19) == -2 );
-
+TEST_CASE("Jogo Impossível 3", "[single-file]") {
+  int teste19[3][3]= {   { 1, 1, 1 },
+                         { 1, 2, 2 },
+                         { 1, 2, 2 }
+  };
+    REQUIRE(VerificaVelha(teste19) == -2);
 }
 
-TEST_CASE( "Jogo Empatado", "[single-file]" ) {
-	int teste20[3][3]= {   { 1, 2, 1 }, 
-	                      { 1, 2, 1 },
-						  { 2, 1, 2 }
-					  };
-    REQUIRE( VerificaVelha(teste20) == 0 );
-
+TEST_CASE("Jogo Empatado", "[single-file]") {
+  int teste20[3][3]= {   { 1, 2, 1 },
+                         { 1, 2, 1 },
+                         { 2, 1, 2 }
+  };
+    REQUIRE(VerificaVelha(teste20) == 0);
 }
 
-TEST_CASE( "Jogo Indefinido 1", "[single-file]" ) {
-	int teste21[3][3]= {   { 1, 2, 0 }, 
-	                      { 1, 2, 1 },
-						  { 2, 1, 2 }
-					  };
-    REQUIRE( VerificaVelha(teste21) == -1 );
-
+TEST_CASE("Jogo Indefinido 1", "[single-file]") {
+  int teste21[3][3]= {   { 1, 2, 0 },
+                         { 1, 2, 1 },
+                         { 2, 1, 2 }
+  };
+    REQUIRE(VerificaVelha(teste21) == -1);
 }
 
-TEST_CASE( "Jogo Indefinido 2", "[single-file]" ) {
-	int teste22[3][3]= {   { 1, 0, 0 }, 
-	                      { 0, 0, 0 },
-						  { 0, 0, 0 }
-					  };
-    REQUIRE( VerificaVelha(teste22) == -1 );
-
+TEST_CASE("Jogo Indefinido 2", "[single-file]") {
+  int teste22[3][3]= {   { 1, 0, 0 },
+                         { 0, 0, 0 },
+                         { 0, 0, 0 }
+  };
+    REQUIRE(VerificaVelha(teste22) == -1);
 }
--- velha.cpp
+++ velha.cpp
@@ -1,140 +1,143 @@
+// Copyright 2021 Pedro Venzi
 /**
  * \file  velha.cpp
  */
 
 
 #include "velha.hpp"
-using namespace std;
 #include <iostream>
 #include <cstdlib>
 
 /** 
- * @brief verifica situacao do jogo da velha
- * @author Programador 
- * @param  velha descreve o parametro
+ * @brief Verifica a Situação do Jogo da Velha
+ * @author Pedro Venzi
+ * @param  velha representa uma imagem estática da disposição de um Jogo da Velha em um dado momento.
  * 
- *  Descrever o que a funcao faz
+ *  Dada uma imagem estática (estado) de um Jogo da Velha, a função verifica o resultado de um jogo
+ *  caso ele já tenha acabado, confere a plausibilidade da aparição de tal estado ao decorrer de um jogo,
+ *  e checa se o jogo ainda está em andamento.
  */ 
 
-int VerificaVelha( int velha[3][3] )
-{	
-	int linha = 0;
-	int coluna = 0;
-	int x_cont = 0;
-	int o_cont = 0;
-	int vazio_cont = 0;
-	int jogadas[9];
-	int n_jogada = 0;
+int VerificaVelha(int velha[3][3]) {
+  int linha = 0;
+  int coluna = 0;
+  int x_cont = 0;
+  int o_cont = 0;
+  int vazio_cont = 0;
+  int jogadas[9];
 
-	for (linha = 0; linha < 3; linha++) {
-		for (coluna = 0; coluna < 3; coluna++) {
-			if (velha[linha][coluna] == 1) {
-				x_cont++;
-			};
-			if (velha[linha][coluna] == 2) {
-				o_cont++;
-			};
-			if (velha[linha][coluna] == 0) {
-				vazio_cont++;
-			};
+  for (linha = 0; linha < 3; linha++) {
+    for (coluna = 0; coluna < 3; coluna++) {
+      // Contagem de X, O, e espaços vazios
+      if (velha[linha][coluna] == 1) {
+        x_cont++;
+      }
+      if (velha[linha][coluna] == 2) {
+        o_cont++;
+      }
+      if (velha[linha][coluna] == 0) {
+        vazio_cont++;
+      }
 
-			if (coluna == 0){
-				jogadas[linha*3] = velha[linha][coluna];
-			};
-			if (coluna == 1){
-				jogadas[linha*3 + 1] = velha[linha][coluna];
-			};
-			if (coluna == 2){
-				jogadas[linha*3 + 2] = velha[linha][coluna];
-			};
-		};
-	};
-	if (abs(x_cont) - abs(o_cont) < 2) {
-		cout << "Array de Jogadas Possível:" << endl;
-		for (n_jogada = 0; n_jogada < 9; n_jogada++) {
-			cout << n_jogada << " - " << jogadas[n_jogada] << endl;
-		};
+      // Preenchimento do Array de Jogadas
+      if (coluna == 0) {
+        jogadas[linha*3] = velha[linha][coluna];
+      }
+      if (coluna == 1) {
+        jogadas[linha*3 + 1] = velha[linha][coluna];
+      }
+      if (coluna == 2) {
+        jogadas[linha*3 + 2] = velha[linha][coluna];
+      }
+    }
+  }
 
-		int condicao_vitoria_x = 0;
-		int condicao_vitoria_o = 0;
+  // Verificação de Jogo em Turnos. Se não estiver em Turnos, Jogo Impossível
+  if (abs(x_cont) - abs(o_cont) < 2) {
+    int condicao_vitoria_x = 0;
+    int condicao_vitoria_o = 0;
 
-		if ((jogadas[2] == jogadas[5]) && (jogadas[5] == jogadas[8])) {
-			if (jogadas[2] == 1) {
-				condicao_vitoria_x++;
-			} else if (jogadas[2] == 2){
-				condicao_vitoria_o++;
-			}
-		}
-		if ((jogadas[1] == jogadas[4]) && (jogadas[4] == jogadas[7])) {
-			if (jogadas[1] == 1) {
-				condicao_vitoria_x++;
-			} else if (jogadas[1] == 2){
-				condicao_vitoria_o++;
-			}
-		}
-		if ((jogadas[0] == jogadas[3]) && (jogadas[3] == jogadas[6])) {
-			if (jogadas[0] == 1) {
-				condicao_vitoria_x++;
-			} else if (jogadas[0] == 2){
-				condicao_vitoria_o++;
-			}
-		}
-		if ((jogadas[0] == jogadas[4]) && (jogadas[4] == jogadas[8])) {
-			if (jogadas[0] == 1) {
-				condicao_vitoria_x++;
-			} else if (jogadas[0] == 2){
-				condicao_vitoria_o++;
-			}
-		}
-		if ((jogadas[2] == jogadas[4]) && (jogadas[4] == jogadas[6])) {
-			if (jogadas[2] == 1) {
-				condicao_vitoria_x++;
-			} else if (jogadas[2] == 2){
-				condicao_vitoria_o++;
-			}
-		}
-		if ((jogadas[6] == jogadas[7]) && (jogadas[7] == jogadas[8])) {
-			if (jogadas[6] == 1) {
-				condicao_vitoria_x++;
-			} else if (jogadas[6] == 2){
-				condicao_vitoria_o++;
-			}
-		}
-		if ((jogadas[3] == jogadas[4]) && (jogadas[4] == jogadas[5])) {
-			if (jogadas[3] == 1) {
-				condicao_vitoria_x++;
-			} else if (jogadas[3] == 2){
-				condicao_vitoria_o++;
-			}
-		}
-		if ((jogadas[0] == jogadas[1]) && (jogadas[1] == jogadas[2])) {
-			if (jogadas[0] == 1) {
-				condicao_vitoria_x++;
-			} else if (jogadas[0] == 2){
-				condicao_vitoria_o++;
-			}
-		}
+    // Contagem de Condições de Vitória
+    if ((jogadas[2] == jogadas[5]) && (jogadas[5] == jogadas[8])) {
+      if (jogadas[2] == 1) {
+        condicao_vitoria_x++;
+      } else if (jogadas[2] == 2) {
+        condicao_vitoria_o++;
+      }
+    }
+    if ((jogadas[1] == jogadas[4]) && (jogadas[4] == jogadas[7])) {
+      if (jogadas[1] == 1) {
+        condicao_vitoria_x++;
+      } else if (jogadas[1] == 2) {
+        condicao_vitoria_o++;
+      }
+    }
+    if ((jogadas[0] == jogadas[3]) && (jogadas[3] == jogadas[6])) {
+      if (jogadas[0] == 1) {
+        condicao_vitoria_x++;
+      } else if (jogadas[0] == 2) {
+        condicao_vitoria_o++;
+      }
+    }
+    if ((jogadas[0] == jogadas[4]) && (jogadas[4] == jogadas[8])) {
+      if (jogadas[0] == 1) {
+        condicao_vitoria_x++;
+      } else if (jogadas[0] == 2) {
+        condicao_vitoria_o++;
+      }
+    }
+    if ((jogadas[2] == jogadas[4]) && (jogadas[4] == jogadas[6])) {
+      if (jogadas[2] == 1) {
+        condicao_vitoria_x++;
+      } else if (jogadas[2] == 2) {
+        condicao_vitoria_o++;
+      }
+    }
+    if ((jogadas[6] == jogadas[7]) && (jogadas[7] == jogadas[8])) {
+      if (jogadas[6] == 1) {
+        condicao_vitoria_x++;
+      } else if (jogadas[6] == 2) {
+        condicao_vitoria_o++;
+      }
+    }
+    if ((jogadas[3] == jogadas[4]) && (jogadas[4] == jogadas[5])) {
+      if (jogadas[3] == 1) {
+        condicao_vitoria_x++;
+      } else if (jogadas[3] == 2) {
+        condicao_vitoria_o++;
+      }
+    }
+    if ((jogadas[0] == jogadas[1]) && (jogadas[1] == jogadas[2])) {
+      if (jogadas[0] == 1) {
+        condicao_vitoria_x++;
+      } else if (jogadas[0] == 2) {
+        condicao_vitoria_o++;
+      }
+    }
 
-		if ((condicao_vitoria_x > 0) || (condicao_vitoria_o > 0)) {
-			if (((condicao_vitoria_x > 1) || (condicao_vitoria_o > 1)) || ((condicao_vitoria_x == 1) && (condicao_vitoria_o == 1))) {
-				return -2;
-			}
-			if (condicao_vitoria_x == 1) {
-				return 1;
-			} else if (condicao_vitoria_o == 1) {
-				return 2;
-			}
-			
-		} else {
-			if (vazio_cont == 0) {
-				return 0;
-			} else {
-				return -1;
-			};
-		};
-	} else {
-		return -2;
-	};
-	return 0;
+    if ((condicao_vitoria_x > 0) || (condicao_vitoria_o > 0)) {
+      // Verificação de Vitórias Simultânes e Duplicadas
+      if (((condicao_vitoria_x > 1) || (condicao_vitoria_o > 1)) ||
+         ((condicao_vitoria_x == 1) && (condicao_vitoria_o == 1))) {
+        return -2;
+      }
+      // Verificação de Vitórias Singulares e Legítimas
+      if (condicao_vitoria_x == 1) {
+        return 1;
+      } else if (condicao_vitoria_o == 1) {
+        return 2;
+      }
+
+    } else {
+      // Verificação de Empates e Indeterminações
+      if (vazio_cont == 0) {
+        return 0;
+      } else {
+        return -1;
+      }
+    }
+  } else {
+    return -2;
+  }
+  return 0;
 }
-
